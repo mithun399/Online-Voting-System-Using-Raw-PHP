49f85fc64f181e5f75f4cf59163bf80c3e75d36f
ms <msovik399@gmail.com>
2022-07-29 08:02:10 +0600

    registation complete

--- register.php
+++ register.php
@@ -14,7 +14,7 @@
     <hr>
     <h3>Registration</h3>
     <div id="body">
-    <form action="#" method="post">
+    <form action="#" method="post" enctype="multipart/form-data">
         <input type="text" name="name" placeholder="Enter Your Name">
         <input type="number" name="mobile" placeholder="Enter Your Mobile"><br><br>
         <input type="password" name="password1" placeholder="Enter Your Password">
@@ -40,3 +40,44 @@
     </div>
 </body>
 </html>
+
+<?php
+include('connect.php');
+$name=$_POST['name'];
+$mobile=$_POST['mobile'];
+$password1=$_POST['password1'];
+$password2=$_POST['password2'];
+$address=$_POST['address'];
+$img=$_FILES['img']['name'];
+$tmp_name=$_FILES['img']['tmp_name'];
+$role=$_POST['role'];
+if ($password1==$password2) {
+    move_uploaded_file($tmp_name,"../img/$img");
+    $insert=mysqli_query($connect,"INSERT INTO user (name,mobile,address,password,photo,role,status,votes) VALUES('$name','$mobile','$address','$password1','$img','$role',0,0)");
+    if($insert){
+        echo '
+        <script>
+        alert("Registration Successfull");
+        window.location="index.php";
+        </script>
+        '; 
+    }
+    else {
+        echo '
+        <script>
+        alert("Failed to register");
+        window.location="register.php";
+        </script>
+        ';
+    }
+} else {
+    echo '
+    <script>
+    alert("password not mathch");
+    window.location="register.php";
+    </script>
+    ';
+}
+
+
+?>
